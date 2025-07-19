7e41860216c00effbf3d40cb6f23ba892dcfc3bf
iamsadiashakil <iamsadiashakil@gmail.com>
2025-07-19 21:28:31 +0500

    Initial commit for Health App

---
+++ src/main/java/com/healthcare/my_health_app/MyHealthAppApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.my_health_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHealthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHealthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthcare/my_health_app/MyHealthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.my_health_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHealthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
